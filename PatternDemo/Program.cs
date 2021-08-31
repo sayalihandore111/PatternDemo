7055d0c61ef8fe865d1b05fe014bc9dd502929c9
sayalihandore111 <sayalihandore111@gmail.com>
2021-08-31 23:20:52 +0530

    Add project files.

---
+++ PatternDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+namespace PatternDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, rows;
+            Console.Write("Input number of rows : ");
+            rows = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= rows; i++)
+            {
+                for (j = 1; j <= i; j++)
+                    Console.Write("{0} ", j);
+                Console.WriteLine();
+            }
+        }
+    }
+}
